b51896e0899ec706c3dc8b86219e67a9ee9c49bb
Rafael Aragão <46876806+rafaelfaragao@users.noreply.github.com>
2021-08-08 21:52:19 -0300

    Conceitos de Cmponente Props Estados e add Fast Refresh Plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { Counter } from './components/Counter';
+import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 
 export function App(){
-  return <h1>Hello World React JS</h1>
+  return (
+    <>
+      <RepositoryList />
+      <Counter />
+    </>
+  );
 }
