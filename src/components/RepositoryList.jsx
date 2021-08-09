053da4e7c35894c7e77f2de981940f0828ead934
Rafael Aragão <46876806+rafaelfaragao@users.noreply.github.com>
2021-08-08 22:33:19 -0300

    Listando dados da Api do Github, aprendendo array.map()

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,21 +1,30 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
-}
+import '../styles/repositories.scss';
+
+//https://api.github.com/orgs/rocketseat/repos
 
 export function RepositoryList(){
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(()=>{
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+    .then(response => response.json())
+    .then(data => setRepositories(data))
+  }, []); 
+  //se passar o valor do array vazio, o useEffect só vai executar uma única vez
+  //nunca usar o useEffect sem passar o segundo parâmetro para não entrar em loop
+  //Não mudar o valor da variável que seja o segundo parâmetro dentro do useEffect é outro loop
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository}/>
+        })}
       </ul>
     </section>
   );
